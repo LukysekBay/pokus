06ebc3408b9e810dc49dd8cbef39b156a80589c6
Lukas Pour <lukaspour@gmail.com>
2016-12-21 12:23:17 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ deploy_cluster() {
 
     make_task_def
     register_definition
-    if [[ $(aws ecs update-service --cluster $family --service $family-service --task-definition $revision | \
+    if [[ $(aws ecs update-service --cluster $AWS_PROJECT_NAME --service $family-service --task-definition $revision | \
                    $JQ '.service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
         return 1
